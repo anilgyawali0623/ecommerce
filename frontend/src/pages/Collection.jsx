27a67333212f35168172c2fe44dbbe8279713f70
anilgyawali0623 <anilgyawali8937@gmail.com>
2024-10-24 18:24:45 +0545

    some changes which i will discard later

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,14 +1,47 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import BannerImage from '../components/BannerImage';
+import React from "react";
+import { useParams } from "react-router-dom";
+import BannerImage from "../components/BannerImage";
+import { CiFilter } from "react-icons/ci";
 
 function Collection() {
-     const {name}= useParams();
+  const { name } = useParams();
   return (
     <div>
-       <BannerImage/>
+      <BannerImage />
+
+      <div className="w-full h-full px-28 ">
+        <div className="w-40 pb-4">
+          <div className="flex justify-between items-center">
+            <h1>filter</h1>
+            <CiFilter />
+          </div>
+          <div className="w-[100%]">
+            <div>
+              <h1 className="py-2 px-1 bg-pink-300 rounded-2xl">Availabilty</h1>
+           
+            <div>
+                <div  className="flex justify-between">
+                  <p>sdasd</p>
+                  <input type="radio" />
+                  <input type="radio" /> 
+                </div>
+                
+                <div  className="flex justify-between">
+                  <p>sdasd</p>
+                </div>
+                <div  className="flex justify-between">
+                  <p>sdasd</p>
+                  <input type="radio" />
+                </div>
+            </div>
+            </div>
+          </div>
+        </div>
+
+        <div></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Collection
+export default Collection;

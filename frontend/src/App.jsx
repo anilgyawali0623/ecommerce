bfeff207d04f13112291ad103aee3c8ffc48ced9
anilgyawali0623 <anilgyawali8937@gmail.com>
2024-10-25 10:16:53 +0545

    signup frontend by clicking the button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "flowbite-react";
 import TopBar from "./components/TopBar";
 import Navbar from "./components/Navbar";
@@ -6,16 +6,28 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Collection from "./pages/Collection";
 import Home from "./pages/Home";
+import Signup from "./components/Signup";
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
   return (
     <div>
       <BrowserRouter>
-      <TopBar />
-      <Navbar />
+        <TopBar />
+        <Navbar toggleModal={toggleModal} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/collections/:name" element={<Collection/>}/>
+          <Route path="/collections/:name" element={<Collection />} />
+          <Route
+            path="/sign-up"
+            element={
+              <Signup isModalOpen={isModalOpen} toggleModal={toggleModal} />
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>

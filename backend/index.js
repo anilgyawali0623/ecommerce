59045f7001a73a6b9220f4f9df9a882c549579df
anilgyawali0623 <anilgyawali8937@gmail.com>
2024-10-27 11:24:19 +0545

    some nodemailer features i got which i will use to send email and all the items purchased by the users

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,34 @@
+// import nodemailer from "nodemailer";
+
+// let transporter = nodemailer.createTransport({
+//   service: 'gmail', // You can use 'Yahoo', 'Outlook', or custom SMTP server
+//   auth: {
+//     user: 'anilgyawali38@gmail.com', // Your email
+//     pass: 'atkh eqpe xdga cijb' // Your email password or App Password
+//   }
+// });
+
+// let mailOptions = {
+//   // from: 'anilgyawali38@gmail.com', // Sender's email
+//   // to: 'anilgyawali244@gmail.com', // List of recipients
+//   subject: 'Hello from rqwrqwrNodemailer', // Subject line
+//   text: 'This is a plain faefqwfqwtext email.', // Plain text body
+
+// };
+
+// transporter.sendMail(mailOptions, (error, info) => {
+//    console.log("fasfasf")
+//   if (error) {
+//     return console.error('Error sending email:', error);
+//   }
+//    console.log("email", info);
+// })
+
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import collectionRoutes from "./routes/collection.route.js";
 import authRoutes from "./routes/auth.route.js";
-
 dotenv.config();
 // console.log(process.env.MONGO_DB);
 mongoose
